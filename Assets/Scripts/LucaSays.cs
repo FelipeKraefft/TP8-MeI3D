9b60114b059b13b5f2af52a752ae6905f04d8f80
FelipeKraefft <101569052+FelipeKraefft@users.noreply.github.com>
2022-09-13 10:55:29 -0300

    UI improvements

---
+++ Assets/Scripts/LucaSays.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LucaSays : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(collider col){
+        if(col.gameObject.tag == "Player"){
+            player.GetComponent<UIManager>().LucaSays.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    void OnTriggerExit(collider col){
+        if(col.gameObject.tag == "Player"){
+            player.GetComponent<UIManager>().LucaSays.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
